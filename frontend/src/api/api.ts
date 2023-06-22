881c63c4e1cdbbdecb81f820135d1a254225b1c6
MrBurnsAI <133540966+MrBurnsAI@users.noreply.github.com>
2023-06-22 16:12:21 +0200

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -16,12 +16,5 @@ export async function conversationApi(options: ConversationRequest, abortSignal:
 }
 
 export async function getUserInfo(): Promise<UserInfo[]> {
-    const response = await fetch('/.auth/me');
-    if (!response.ok) {
-        console.log("No identity provider found. Access to chat will be blocked.")
-        return [];
-    }
-
-    const payload = await response.json();
-    return payload;
+    return [];
 }
